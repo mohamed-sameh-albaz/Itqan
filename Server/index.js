a78dd8a2100f73bbe2758b1c5000ca484de1bff2
mohamed-sameh-albaz <136837275+mohamed-sameh-albaz@users.noreply.github.com>
2024-12-14 10:10:54 +0200

    handler for default routing

--- Server/index.js
+++ Server/index.js
@@ -6,7 +6,7 @@ const contestRoutes = require("./src/routes/contestRoutes");
 const communityRoutes = require("./src/routes/communityRoutes");
 const groupRoutes = require("./src/routes/groupRoutes");
 const teamRoutes = require("./src/routes/teamRoutes");
-
+const httpStatusText = require("./src/utils/httpStatusText");
 // Load environment variables from .env file
 dotenv.config();
 
@@ -19,6 +19,9 @@ app.use("/api/communities", communityRoutes);
 app.use("/api/groups", groupRoutes);
 app.use("/api/teams", teamRoutes);
 
+app.all('*', (req, res, next) => {
+  return res.status(404).json({status: httpStatusText.ERROR, message: 'This Resource Is Not Found'});
+})
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
