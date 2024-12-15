154e27ca29af460a70b430c40f642e3ab6cd95c6
ZiadMontaser <ziadmontaser2005@gmail.com>
2024-12-15 05:35:34 +0200

    Refactor CommunityNavBar and PostsPage styles; update routing and API calls in TeamEditor and GroupPage

--- Client/src/App.js
+++ Client/src/App.js
@@ -23,15 +23,15 @@ function App() {
           <Route path="/auth" element={<Auth />} />
           <Route path="/home" element={<HomePage />} />
           <Route path="/community/:name" element={<CommunityPage />} />
-          <Route path="/group" element={<GroupPage />} />
+          <Route path="/community/:name/group/:id" element={<GroupPage />} />
           <Route path="/contest" element={<ContestPage />} />
-          <Route path="/teame" element={<TeamEditor />} />
+          <Route path="/team/create" element={<TeamEditor />} />
           <Route path="/teams" element={<TeamsPage />} />
 
           <Route path="/add-post" element={<AddPostPage />} /> {/* Add the new route */}
 
           <Route path="/profile/:name" element={<ProfilePage/>} /> 
-          <Route path="/posts/:name" element={<PostsPage/>} /> 
+          <Route path="/community/:name/posts" element={<PostsPage/>} /> 
 
         </Routes>
       </div>
--- Client/src/views/TeamEditor.js
+++ Client/src/views/TeamEditor.js
@@ -1,7 +1,11 @@
 import TeamEditorBlock from "../components/TeamEditorBlock";
 import CommunityNavBar from "../components/CommunityNavBar";
 import "./TeamEditor.css"
+import { useLocation } from "react-router-dom";
 const TeamEditor = () => {
+    const location = useLocation();
+    const communityName = location.state || {};
+    console.log("Community Name", communityName);
     return (
 
         <div>
